58e6cfe18a5704f5eb6c191588639bdb76270f4e
Luiz Moura <muniz.mourafelipe@gmail.com>
2024-04-01 22:52:32 -0300

    Update zone.js cdn

--- src/Chef-root-config.ts
+++ src/Chef-root-config.ts
@@ -5,9 +5,17 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
-import "zone.js";
+import { HTMLLayoutData } from "single-spa-layout/dist/types/isomorphic/constructRoutes";
 
-const routes = constructRoutes(microfrontendLayout);
+const htmlLayoutData: HTMLLayoutData = {
+  errors: {
+    errorTemplate: "<h1>Error</h1>"
+  },
+  loaders: {},
+  props: {}
+}
+
+const routes = constructRoutes(microfrontendLayout, htmlLayoutData);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
